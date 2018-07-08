6dc1f0e9ca472f3c1bf3c1ee74a345d9f3164128
Venus Kong <venus@Vinces-MacBook-Air.local>
2018-07-08 01:41:42 -0400

    Ajax can now send form data to server as value pairs, server can return a message according to this form data, server can return message about whether the user's answer is right or wrong according to this data, this response gets displayed in a div on the home page. ie, the note recognition function is successful

--- back_end/server.js
+++ back_end/server.js
@@ -1,12 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
-const earTuner = require('./views/playSounds.js');
 const app = express();
 var fs = require('fs');
-var http = require("http");
-var url = require("url");
-var mime = require('mime');
 
 const HTTP_PORT = process.env.PORT || 8080;
 
@@ -15,43 +11,16 @@ function onHttpStart() {
     console.log("Express http server listening on: " + HTTP_PORT);
   }
 
-/** bodyParser.urlencoded(options)
- * Parses the text as URL encoded data (which is how browsers tend to send form data from regular forms set to POST)
- * and exposes the resulting object (containing the keys and values) on req.body
- */
+
+// Parses the text as URL encoded data and exposes the resulting object (containing the keys and values) on req.body
 app.use(bodyParser.urlencoded({extended: true}));
 
-/**bodyParser.json(options)
- * Parses the text as JSON and exposes the resulting object on req.body.
- */
+// Parses the text as JSON and exposes the resulting object on req.body
 app.use(bodyParser.json());
 
 
-// how do you make your audio file available for the browser?
+// make your audio file available for the browser
 app.use(express.static('pianoNotes'));
-
-
-/*
-http.createServer(function(request, response) {
-   var pathname = url.parse(request.url).pathname;
-   console.log("Request for " + pathname + " received.");
-
-   response.writeHead(200);
-
-   if(pathname == "/") {
-       html = fs.readFileSync("back_end/views/home.html", "utf8");
-       response.write(html);
-   } else if (pathname == "/earTuner.js") {
-       script = fs.readFileSync("back_end/views/earTuner.js", "utf8");
-       response.write(script);
-   } else if (pathname =="/piano-c.wav") {
-       wav = fs.readFileSync("pianoNotes/piano-c.wav", "utf8");
-       response.write(wav);
-   }
-
-   response.end();
-}).listen(HTTP_PORT, onHttpStart);
-*/
 
 app.get("/", (req, res) =>  {
     html = fs.readFileSync("back_end/views/home.html", "utf8");
@@ -61,50 +30,24 @@ app.get("/", (req, res) =>  {
 app.get("/playSounds.js", (req, res) => {
     script = fs.readFileSync("back_end/views/playSounds.js", "utf8");
     res.send(script);
-})
-
-
-app.post("/register", function (req, res) {
-    const formData = req.body;
-    var isCorrect;
-    if (formData.action == formData.correctAnswer) {
-        isCorrect = true;
-    } else {
-        isCorrect = false;
-    }
-    var dataReceived = JSON.stringify(formData);
-    if (isCorrect) {
-        dataReceived += "<br/>Your answer was:<br/>" + "correct"+ "<br/>";
-        helper.makeRequest("Y");
-    } else {
-        dataReceived += "<br/>Your answer was:<br/>" + "wrong" + "<br/>";
-        helper.makeRequest("N");
-    }
-    
-    res.send(dataReceived);
 });
 
-/*
-app.get("/register", function(req, res) {
-    if (req.query.answer == "Y") {
-        res.json({"answer":"answer is correct"});
-    }
-    if (req.query.answer == "N") {
-        res.json({"answer": "answer is incorrect"});
-    }
-});
-*/
-
-/*test*/
-app.get("/api/users", (req, res) => {
-    res.json({message: "fetch all users"});
+app.get("/ajax.js", (req, res) => {
+    script = fs.readFileSync("back_end/views/ajax.js", "utf8");
+    res.send(script);
 });
 
 app.post("/api/users", (req, res) => {
     console.log(req.body);
-    res.json({message: "Your answer is: " + JSON.stringify(req.body) + ". The correct answer is: " + req.body.name});
-});
+    //res.json({message: "Your answer is: " + req.body.answer + ". The correct answer is: " + req.body.correctAnswer});
+    if (req.body.answer == req.body.correctAnswer) {
+        //res.json({message: "Yes you got it!"});
+        res.send("Yes you got it!");
 
-/*end of test*/
+    } else {
+        //res.json({message: "Opps! That wasn't it."});
+        res.send("Opps! That wasn't it.");
+    }
+});
 
 app.listen(HTTP_PORT, onHttpStart);
